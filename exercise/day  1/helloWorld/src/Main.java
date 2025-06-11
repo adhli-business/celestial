9953ab0450299e59c7a2239519f8c2242141ec69
Putra Adhli Falah <71480@fif.co.id>
2025-06-11 12:11:17 +0700

    :white_check_mark: add operators in java

--- exercise/day  1/helloWorld/src/Main.java
+++ exercise/day  1/helloWorld/src/Main.java
@@ -3,33 +3,63 @@ import java.lang.reflect.Array;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello World");
+//
+//        //long
+//        long uangSaya = 200000;
+//        System.out.println(uangSaya);
+//
+//        //integer
+//        int age = 25;
+//        System.out.println(age);
+//
+//        //float dan double
+//        float price = 19.99f;
+//        System.out.println(price);
+//
+//        //character
+//        char grade = 'A';
+//        System.out.println(grade);
+//
+//        //boolean
+//        boolean isTrue = true;
+//        System.out.println(isTrue);
+//
+//        //String
+//        String name = "Adhli";
+//        System.out.println(name);
+//
+//        //Array
+//        int[] numbers = {1,2,3,4};
+//        System.out.println(java.util.Arrays.toString(numbers));
 
-        //long
-        long uangSaya = 200000;
-        System.out.println(uangSaya);
+        // Arithmatic Operators
+        int bilPertama = 10;
+        int bilKedua = 5;
+        int penjumlahan = bilPertama + bilKedua;
+        System.out.println("Hasil penjumlahan dari "+ bilPertama + "+" + bilKedua + "=" + penjumlahan);
 
-        //integer
-        int age = 25;
-        System.out.println(age);
+        int bilKetiga = 15;
+        int bilKeempat = 5;
+        int sisaBagi = bilKetiga % bilKeempat;
+        System.out.println("Sisa hasil bagi dari "+ bilKetiga + "%" + bilKeempat + "=" + sisaBagi);
 
-        //float dan double
-        float price = 19.99f;
-        System.out.println(price);
+        //Relational Operators
+        boolean hasil = (10!=5);
+        System.out.println("Apakah 10 tidak sama dengan 5? " + hasil);
+        boolean hasil2 = (10==5);
+        System.out.println("Apakah 10 sama dengan 5? " + hasil2);
 
-        //character
-        char grade = 'A';
-        System.out.println(grade);
+        //Logical Operators
+        boolean result = (10>5) && (4<6);
+        System.out.println("Apakah 10>5 dan 4<6? " + result);
+        boolean result2 = (10>5) || (4>6);
+        System.out.println("Apakah 10>5 atau 4>6? " + result2);
 
-        //boolean
-        boolean isTrue = true;
-        System.out.println(isTrue);
+        //Bitwise Operators
+        int coba = 5 & 3;
+        int coba2 = 5 | 3;
+        System.out.println(coba);
+        System.out.println(coba2);
 
-        //String
-        String name = "Adhli";
-        System.out.println(name);
-
-        //Array
-        int[] numbers = {1,2,3,4};
-        System.out.println(java.util.Arrays.toString(numbers));
     }
 }
