b9af27aea45f14dfe493e0623f969a0ae0ec17f1
Putra Adhli Falah <71480@fif.co.id>
2025-06-11 11:50:59 +0700

    :white_check_mark: add data types in java

--- exercise/day  1/helloWorld/src/Main.java
+++ exercise/day  1/helloWorld/src/Main.java
@@ -1,5 +1,35 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+
+        //long
+        long uangSaya = 200000;
+        System.out.println(uangSaya);
+
+        //integer
+        int age = 25;
+        System.out.println(age);
+
+        //float dan double
+        float price = 19.99f;
+        System.out.println(price);
+
+        //character
+        char grade = 'A';
+        System.out.println(grade);
+
+        //boolean
+        boolean isTrue = true;
+        System.out.println(isTrue);
+
+        //String
+        String name = "Adhli";
+        System.out.println(name);
+
+        //Array
+        int[] numbers = {1,2,3,4};
+        System.out.println(java.util.Arrays.toString(numbers));
     }
 }
