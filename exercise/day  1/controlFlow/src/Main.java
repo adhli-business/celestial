06325575fbd3ac9dcae64692459b7b79921d4cff
Putra Adhli Falah <71480@fif.co.id>
2025-06-11 14:35:41 +0700

    :white_check_mark: add exercise factorial

--- exercise/day  1/controlFlow/src/Main.java
+++ exercise/day  1/controlFlow/src/Main.java
@@ -1,57 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        //if else
-        String nama = "putra";
-        if (nama == "adhli"){
-            System.out.println("Nama: Adhli");
-        } else if (nama == "putra") {
-            System.out.println("Nama: Putra");
-        } else {
-            System.out.println("Nama: Falah");
+        for (int i = 2; i <= 20; i += 2) {
+            long factorial = 1;
+
+            for (int j = 1; j <= i; j++) {
+                factorial *= j;
+            }
+
+            System.out.println("Faktorial dari " + i + " : " + factorial);
         }
 
-        // Switch Case
-        int angka = 3;
-        String namaHari;
-        switch (angka){
-                case 1:
-                namaHari = "Senin";
-                break;
-
-                case 2:
-                namaHari = "Selasa";
-                break;
-
-                case 3:
-                namaHari = "Rabu";
-                break;
-
-                case 4:
-                namaHari = "Kamis";
-                break;
-
-                case 5:
-                namaHari = "Jumat";
-                break;
-            default:
-                namaHari = "Salah";
-                break;
-        }
-
-        System.out.println("Hari ketiga adalah hari: " + namaHari);
-
-        //While loop
-        int number = 1;
-        while (number<=10) {
-            System.out.println("While loop ke-" + number);
-            number++;
-        }
-
-        //Do While
-        int numbers = 1;
-        do {
-            System.out.println("Do-while ke-" + numbers);
-            numbers++;
-        } while (numbers <=10);
+        //        //if else
+//        String nama = "putra";
+//        if (nama == "adhli"){
+//            System.out.println("Nama: Adhli");
+//        } else if (nama == "putra") {
+//            System.out.println("Nama: Putra");
+//        } else {
+//            System.out.println("Nama: Falah");
+//        }
+//
+//        // Switch Case
+//        int angka = 3;
+//        String namaHari;
+//        switch (angka){
+//                case 1:
+//                namaHari = "Senin";
+//                break;
+//
+//                case 2:
+//                namaHari = "Selasa";
+//                break;
+//
+//                case 3:
+//                namaHari = "Rabu";
+//                break;
+//
+//                case 4:
+//                namaHari = "Kamis";
+//                break;
+//
+//                case 5:
+//                namaHari = "Jumat";
+//                break;
+//            default:
+//                namaHari = "Salah";
+//                break;
+//        }
+//
+//        System.out.println("Hari ketiga adalah hari: " + namaHari);
+//
+//        //While loop
+//        int number = 1;
+//        while (number<=10) {
+//            System.out.println("While loop ke-" + number);
+//            number++;
+//        }
+//
+//        //Do While
+//        int numbers = 1;
+//        do {
+//            System.out.println("Do-while ke-" + numbers);
+//            numbers++;
+//        } while (numbers <=10);
     }
 }
